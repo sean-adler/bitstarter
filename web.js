889c381f23ad2f80e9e4aa10b4f397eb357f1735
Sean Adler <sadler13@cmc.edu>
2013-07-09 18:38:56 -0500

    add index.html bs

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = new Buffer(256);
+  var length = buf.write(fs.readFileSync('index.html', 'utf8'));
+  var respStr = buf.toString('utf8', 0, length);
+  response.send(respStr);
 });
 
 var port = process.env.PORT || 5000;
